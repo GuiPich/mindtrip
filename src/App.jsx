8036d37f0b6fbfb42e0d7623f924d6214d0b298e
Edouard de Champeaux <bobdechampeaux@gmail.com>
2022-11-24 12:31:35 +0100

    routes crées

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home';
+import TonTrip from './Pages/TonTrip';
+import Commande from './Pages/Commande';
 
 function App() {
   return (
     <div className="App">
-    
+
+
+
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/tontrip" element={<TonTrip />} />
+        <Route path="/commande" element={<Commande />} />
+      </Routes>
     </div>
   );
 }
