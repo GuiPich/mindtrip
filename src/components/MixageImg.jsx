522a410bf40340657a59e7af7cabe9e3fbc2a670
GuiPich <110974962+GuiPich@users.noreply.github.com>
2022-11-24 12:21:49 +0100

    essai merge img

---
+++ src/components/MixageImg.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import mergeImages from 'merge-images';
+import landscape from '../assets/MixageImg/landscape.jpg';
+import sheep from '../assets/MixageImg/sheep.jpg';
+
+const MixageImg = () => {
+
+    const [src, setSrc] = useState();
+
+    const createImgMerge = () => {
+        mergeImages([landscape, sheep])
+            .then((res) => {
+                setSrc(res)
+            })
+            .catch((err) => {
+                console.error(err);
+            })
+    };
+
+    useEffect(() => {
+        createImgMerge()
+    }, [])
+
+
+    return (
+        <div className='mixageImg'>
+            <h4>essai</h4>
+            <img src={src} alt="essai" />
+        </div>
+    )
+}
+
+export default MixageImg;
