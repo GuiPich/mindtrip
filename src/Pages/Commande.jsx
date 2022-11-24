ae2d49c5b93185aa1603ffb57d3ba7295920c1a9
Adam Giromanay <adam202009@live.fr>
2022-11-24 19:56:37 +0100

    navbar qui fait mal aux yeux

--- src/Pages/Commande.jsx
+++ src/Pages/Commande.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../components/navbar/Navbar';
 
 function Commande() {
+
+
     return (
-        <div>Commande</div>
+        <>
+            <Navbar />
+            <div className='base'>
+
+                <div className='box'>
+                    <p> Ici je dois rappeler le composant de Vincent pour faire le recap de la selection de la page </p>
+                    <h2> Make your dream come true </h2>
+                    <p> La fusion des images selectionnées </p>
+                </div>
+
+            </div>
+        </>
     )
 }
 
-export default Commande
+
+export default Commande;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../components/navbar/Navbar';
 import video from "../assets/videos/pexels-cottonbro-9669049.mp4";
 
 function Home() {
     return (
         <div className='home'>
-            <div className='video-home'>
-                <video width="100%" autoPlay muted loop>
-                    <source src={video} type="video/mp4" />
-                </video>
-            </div>
+            <Navbar />
+                <div className='video-home'>
+
+                    <video width="100%" autoPlay muted loop>
+                        <source src={video} type="video/mp4" />
+                    </video>
+
+                </div>
+
         </div>
     )
 }
 
-export default Home
+export default Home;
